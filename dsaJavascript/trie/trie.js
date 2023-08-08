7a2ae897d6ee20207c2d879e7472646a9d8f1203
Ajay <ajayzyan100@gmail.com>
2023-08-08 12:08:00 +0530

    trie 🦖

---
+++ dsaJavascript/trie/trie.js
@@ -0,0 +1,56 @@
+class TrieNode {
+  constructor() {
+    this.children = {};
+    this.isEndOfWord = false;
+  }
+}
+
+class Trie {
+  constructor() {
+    this.root = new TrieNode();
+  }
+
+  insert(word) {
+    let node = this.root;
+    for (const char of word) {
+      if (!node.children[char]) {
+        node.children[char] = new TrieNode();
+      }
+      node = node.children[char];
+    }
+    node.isEndOfWord = true;
+  }
+
+  search(word) {
+    let node = this.root;
+    for (const char of word) {
+      if (!node.children[char]) {
+        return false;
+      }
+      node = node.children[char];
+    }
+
+    return node.isEndOfWord;
+  }
+
+  startsWith(prefix) {
+    let node = this.root;
+    for (const char of prefix) {
+      if (!node.children[char]) {
+        return false;
+      }
+      node = node.children[char];
+    }
+    return true;
+  }
+}
+
+const t = new Trie();
+
+t.insert('Ajay');
+t.insert('Ajmal');
+t.insert('aju');
+t.insert('apple');
+
+console.log(t.search('aju'));
+console.log(t.startsWith('ad'));
