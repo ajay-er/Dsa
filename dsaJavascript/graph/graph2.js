a692f39ea89d29ce3a378ffc3f2378f2307797f2
Ajay <ajayzyan100@gmail.com>
2023-08-08 11:33:47 +0530

    bfs dfs 🦕

--- dsaJavascript/graph/graph2.js
+++ dsaJavascript/graph/graph2.js
@@ -22,11 +22,76 @@ class Graph {
   removeVertex(vertex) {
     while (this.adjacencyList[vertex].length) {
       let vertex2 = this.adjacencyList[vertex].pop();
-      this.removeEdge(vertex,vertex2)
+      this.removeEdge(vertex, vertex2);
     }
 
-    delete this.adjacencyList[vertex]
+    delete this.adjacencyList[vertex];
+  }
 
+  //using recursive method
+  depthFirstRecursive(start) {
+    const result = [];
+    const visitedVertices = {};
+    this.dfs(start, visitedVertices, result);
+    return result;
+  }
+
+  dfs(vertex, visitedVertices, result) {
+    if (visitedVertices[vertex]) return;
+
+    visitedVertices[vertex] = true;
+    result.push(vertex);
+
+    for (const neighborVertex of this.adjacencyList[vertex]) {
+      if (!visitedVertices[neighborVertex])
+        this.dfs(neighborVertex, visitedVertices, result);
+    }
+  }
+
+  //using iterative method
+  depthFirstIterative(start) {
+    const stack = [start];
+    const result = [];
+    const visitedVertices = {};
+    let currentVertex;
+
+    visitedVertices[start] = true;
+    while (stack.length) {
+      currentVertex = stack.pop();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visitedVertices[neighbor]) {
+          visitedVertices[neighbor] = true;
+          stack.push(neighbor);
+        }
+      });
+    }
+
+    return result;
+  }
+
+  breadthFirstSearch(start) {
+    const queue = [start];
+    const result = [];
+    const visitedVertices = {};
+
+    let currentVertex;
+    visitedVertices[start] = true;
+
+    while (queue.length) {
+      currentVertex = queue.shift();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visitedVertices[neighbor]) {
+          visitedVertices[neighbor] = true;
+          queue.push(neighbor);
+        }
+      });
+    }
+
+    return result;
   }
 }
 
@@ -34,9 +99,17 @@ let g = new Graph();
 
 g.addVertex('ajay');
 g.addVertex('sreerag');
+g.addVertex('unni');
+g.addVertex('kumar');
+g.addVertex('ajith');
+g.addVertex('sachin');
+g.addEdge('unni', 'kumar');
+g.addEdge('ajay', 'unni');
+g.addEdge('ajay', 'kumar');
 g.addEdge('ajay', 'sreerag');
+g.addEdge('sachin', 'sreerag');
+g.addEdge('ajith', 'sachin');
 
 console.log(g.adjacencyList);
 
-g.removeVertex('ajay');
-console.log(g.adjacencyList);
+console.log(g.breadthFirstSearch('ajay'));
