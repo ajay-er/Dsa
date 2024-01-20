ae73c1017a1f3b8e64d1663b1ffa4db567bea89d
ajay-er <ajayzyan100@gmail.com>
2024-01-20 15:36:32 +0530

    🐳

---
+++ dsaJavascript/workout-boarding/sorting/selection1.js
@@ -0,0 +1,22 @@
+function selection(arr) {
+  let min = 0;
+  for (let i = 0; i < arr.length; i++) {
+    min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[min] > arr[j]) {
+        min = j;
+      }
+    }
+    swap(arr,min,i);
+  }
+}
+
+function swap(arr, i, j) {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+let arr = [2, 34, 5, 12, 3, -2, 1];
+selection(arr);
+console.log(arr);
