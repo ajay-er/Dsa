0fd43595306d95bdf4ad35ddb55d7328c6914006
ajay-er <ajayzyan100@gmail.com>
2024-02-26 12:50:32 +0530

    rotated array bs

---
+++ dsaJavascript/revise/bs.js
@@ -0,0 +1,30 @@
+var search = function (nums, target) {
+  let start = 0;
+  let end = nums.length - 1;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    }
+
+    if (nums[start] <= nums[mid]) {
+      if (nums[start] <= target && target < nums[mid]) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+    } else {
+      if (nums[mid] < target && target <= nums[end]) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    }
+  }
+  return -1;
+};
+let nums = [6, 7, 1, 2, 3, 4, 5];
+let target = 5;
+
+console.log(search(nums, target));
